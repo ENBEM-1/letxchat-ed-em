19a600deda6f99cd0d8681289746771768ab53e1
emensah <100359584+ENBEM-1@users.noreply.github.com>
2022-10-28 13:02:00 +0000

    hm

--- frontend/src/components/MessageForm.js
+++ frontend/src/components/MessageForm.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { Button, Col, Form, Row,Modal } from "react-bootstrap";
+import { Button, Col, Form, Row, Modal } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { AppContext } from "../context/appContext";
 import "./MessageForm.css";
 
 function MessageForm() {
+
     const [message, setMessage] = useState("");
     const [file, setFile] = useState("");
     const [url, setUrl] = useState("");
@@ -13,34 +14,37 @@ function MessageForm() {
     const user = useSelector((state) => state.user);
     const { socket, currentRoom, setMessages, messages, privateMemberMsg } = useContext(AppContext);
     const messageEndRef = useRef(null);
+
+
+
     function MyVerticallyCenteredModal(props) {
         return (
-          <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                image preview
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className='d-flex align-items-center mb-3 preview'>
-                <img width={"100%"} src={url} alt='' />
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        image preview
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='d-flex align-items-center mb-3 preview'>
+                        <img width={"100%"} src={url} alt='' />
 
-                </div>
-              
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-          </Modal>
+                    </div>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         );
-      }
-      
-    
+    }
+
+
     useEffect(() => {
         scrollToBottom();
     }, [messages]);
@@ -69,59 +73,55 @@ function MessageForm() {
         setMessages(roomMessages);
     });
     function selectFile(e) {
-           console.log(e.target.file[0]);
+        // console.log(e.target.file[0]);
         setMessage(e.target.files[0].name);
         setFile(e.target.files[0]);
-        }
-        
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         if (!message) return;
-        
+
         const today = new Date();
         const minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
         const time = today.getHours() + ":" + minutes;
         const roomId = currentRoom;
         console.log(file);
-        socket.emit("message-room", roomId, message, user, time, todayDate,file,
-       file ?  file.type :'text');
+        socket.emit("message-room", roomId, message, user, time, todayDate, file,
+            file ? file.type : 'text');
 
         setMessage("");
         setFile();
+    }
+
+    function image(url) {
+        return (<img onClick={() => {
+            setModalShow(true);
+            setUrl(url);
+        }} width={"100%"} height={200} alt={url} src={url} />
+        )
+    }
+
+    function video(url) {
+        return (<video width={"100%"} height={200} alt={url} src={url} />)
+    }
+    function application(url) {
+        return (<a download target="_blank" rel="noreferrer" href={url}>download</a>)
+    }
+
+    function checkType(type) {
+        if (!type) return "text";
+        return type.split("/")[0];
 
     }
 
 
-
-    
-function image(url) {
-    return (<img onClick={() => {
-        setModalShow(true);
-        setUrl(url);
-    }} width={"100%"} height={200} alt={url} src={url} />    
-    )   
-}
-
-function video(url) {
-    return (<video width={"100%"} height={200} alt={url} src={url} />)   
-}
-function application(url) {
-    return (<a download target="_blank" rel="noreferrer" href={url}>download</a>)   
-}
-
-function checkType(type) {
-    if(!type) return "text";
-    return type.split("/")[0];
-    
-}
-
-
     return (
         <>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
             <div className="messages-output">
                 {user && !privateMemberMsg?._id && <div className="alert alert-info">You are in the {currentRoom} room</div>}
                 {user && privateMemberMsg?._id && (
@@ -139,33 +139,37 @@ function checkType(type) {
                     messages.map(({ _id: date, messagesByDate }, idx) => (
                         <div key={idx}>
                             <p className="alert alert-info text-center message-date-indicator">{date}</p>
-                            {messagesByDate?.map(({ content, time, from: sender,file,type }, msgIdx) => (
+                            {messagesByDate?.map(({ content, time, from: sender, file, type }, msgIdx) => (
                                 <div className={sender?.email === user?.email ? "message" : "incoming-message"} key={msgIdx}>
                                     <div className="message-inner">
                                         <div className="d-flex align-items-center mb-3">
                                             <img src={sender.picture} style={{ width: 35, height: 35, objectFit: "cover", borderRadius: "50%", marginRight: 10 }} alt='' />
                                             <p className="message-sender">{sender._id === user?._id ? "You" : sender.name}</p>
                                         </div>
-                                        {type !== "text"? checkType(type) === "image"?image(file):checkType(type) === "video"?video(file):application(file):null}
-                                        <div className='message-content'>
-                                    <div>
-                                    {  
-                                        content.replace('\\', '/').substring(0, 8) === "uploads/"?
-                                        content.replace('\\', '/').substring(content.length - 3, content.length) === 'mp4'?
-                                        <video 
-                                        style={{maxWidth:'200px'}}
-                                        src={`https://letxchat-ed-em.herokuapp.com/${content}`} 
-                                        alt='video' type="video/mp4" controls/>
-                                        :
-                                        <img 
-                                        style={{maxWidth:'200px'}}
-                                        src={`https://letxchat-ed-em.herokuapp.com/${content}`} alt='img'/>
-                                        :
-                                        <p>{content}</p>  
-                                    }
-                                    <p className='message-timestamp-left'>{time}</p> 
+                                        {type !== "text" ? checkType(type) === "image" ? image(file) : checkType(type) === "video" ? video(file) : application(file) : null}
+
+                                        <p className="message-content">{content}</p>
+                                        <p className='message-timestamp-left'>{time}</p>
+
+                                        {/* <div className='message-content'>
+                                            <div>
+                                                {
+                                                    content.replace('\\', '/').substring(0, 8) === "uploads/" ?
+                                                        content.replace('\\', '/').substring(content.length - 3, content.length) === 'mp4' ?
+                                                            <video
+                                                                style={{ maxWidth: '200px' }}
+                                                                src={`http://letxchat-ed-em.herokuapp.com/${content}`}
+                                                                alt='video' type="video/mp4" controls />
+                                                            :
+                                                            <img
+                                                                style={{ maxWidth: '200px' }}
+                                                                src={`http://letxchat-ed-em.herokuapp.com/${content}`} alt='img' />
+                                                        :
+                                                        <p>{content}</p>
+                                                }
+                                            </div>
+                                        </div> */}
                                     </div>
-                                    </div></div>
                                 </div>
                             ))}
                         </div>
@@ -174,7 +178,7 @@ function checkType(type) {
             </div>
             <Form onSubmit={handleSubmit}>
                 <Row>
-                    
+
                     <Col md={10}>
                         <Form.Group>
                             <Form.Control id="text-box" type="text" placeholder="Your message" disabled={!user} value={message} onChange={(e) => setMessage(e.target.value)}></Form.Control>
@@ -182,8 +186,8 @@ function checkType(type) {
 
                     </Col>
                     <Col md={1}>
-                        <input onChange={selectFile} type='file'/>
-                            <Button type="submit" style={{ width: "100%", backgroundColor: "#f88a2a", borderColor: '#f88a2a' }} disabled={!user}>
+                        <input onChange={selectFile} type='file' />
+                        <Button type="submit" style={{ width: "100%", backgroundColor: "#f88a2a", borderColor: '#f88a2a' }} disabled={!user}>
                             <i class="fa fa-upload"></i>
                         </Button>
                     </Col>
@@ -196,9 +200,9 @@ function checkType(type) {
             </Form>
 
             <MyVerticallyCenteredModal
-        show={false}
-        onHide={() => setModalShow(false)}
-      />
+                show={false}
+                onHide={() => setModalShow(false)}
+            />
         </>
     );
 }
